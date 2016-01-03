47d728286853afa0ae224ddd8d7c403c328f1d77
deonjtan@gmail.com <deonjtan@gmail.com>
2016-01-03 17:35:42 +0000

    working list in fragment

--- FurnituAR/app/src/main/java/com/example/deon/furnituar/BrowseFurnitureActivity.java
+++ FurnituAR/app/src/main/java/com/example/deon/furnituar/BrowseFurnitureActivity.java
@@ -1,17 +1,13 @@
 package com.example.deon.furnituar;
 
 import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,38 +26,33 @@ public class BrowseFurnitureActivity extends AppCompatActivity {
                    .commit();
        }
     }
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle)
 
     public static class PlaceholderFragment extends Fragment {
-        ArrayAdapter<String> furnitureListAdapter;
+        private ArrayAdapter<String> furnitureListAdapter;
         public PlaceholderFragment() {
         }
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-//            String[] tableList = {
-//                    "table 1",
-//                    "table 2",
-//                    "table 3",
-//                    "table 4",
-//                    "table 5",
-//                    "table 6"
-//            };
-//            List<String> furnitureList = new ArrayList<String>(Arrays.asList(tableList));
-//            furnitureListAdapter = new ArrayAdapter<String>(
-//                    getActivity(),
-//                    R.layout.content_browse_furniture,
-//                    R.id.text_view_furniture_description,
-//                    furnitureList);
-        }
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_browse_furniture, container, false);
+            String[] tableList = {
+                   "table 1",
+                   "table 2",
+                   "table 3",
+                   "table 4",
+                   "table 5",
+                   "table 6"};
 
-//            TextView furnitureListView = (TextView) rootView.findViewById(R.id.text_view_furniture);
-//            furnitureListView.setAdapter(furnitureListAdapter);
-//            ((ViewGroup)furnitureListView.getParent()).removeView(furnitureListView);
+           List<String> furnitureList = new ArrayList<String>(Arrays.asList(tableList));
+           furnitureListAdapter = new ArrayAdapter<String>(
+                   getActivity(),
+                   R.layout.content_browse_furniture,
+                   R.id.text_view_furniture_description,
+                   furnitureList);
+
+            View rootView = inflater.inflate(R.layout.fragment_browse_furniture, container, false);
+            ListView furniture_listView = (ListView) rootView.findViewById(R.id.list_view_furniture);
+            furniture_listView.setAdapter(furnitureListAdapter);
+
             return rootView;
         }
 
