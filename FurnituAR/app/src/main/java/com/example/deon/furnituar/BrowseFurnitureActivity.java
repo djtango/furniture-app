f518d31ef81146351bacd0809980dfeb97e29a66
Rajeev Hejib <rajeev_hejib@yahoo.com>
2016-01-05 12:14:51 +0000

    custom-list complete (w/o styling)

--- FurnituAR/app/src/main/java/com/example/deon/furnituar/BrowseFurnitureActivity.java
+++ FurnituAR/app/src/main/java/com/example/deon/furnituar/BrowseFurnitureActivity.java
@@ -1,9 +1,13 @@
 package com.example.deon.furnituar;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -45,108 +49,30 @@ public class BrowseFurnitureActivity extends AppCompatActivity{
         } ;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_browse_furniture);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_furniture);
 
-            CustomList adapter = new
-                    CustomList(BrowseFurnitureActivity.this, item, imageId, desc);
-            list=(ListView)findViewById(R.id.list);
-            list.setAdapter(adapter);
-            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        CustomList adapter = new
+                CustomList(BrowseFurnitureActivity.this, item, imageId, desc);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-                    Toast.makeText(BrowseFurnitureActivity.this, "You Clicked at " + item[+position], Toast.LENGTH_SHORT).show();
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
 
-                }
-            });
+                String selection = item[+position];
+                Intent loadRenderFurnitureActivity = new Intent(BrowseFurnitureActivity.this, RenderFurnitureActivity.class);
 
-        }
+                loadRenderFurnitureActivity.putExtra(SELECTED_FURNITURE, selection);
+                startActivity(loadRenderFurnitureActivity);
+
+            }
+        });
 
     }
 
-
-
-
-
-
-//
-//
-//import android.content.Intent;
-//import android.support.v4.app.Fragment;
-//import android.util.Log;
-//import android.view.LayoutInflater;
-//import android.view.ViewGroup;
-//import android.widget.ArrayAdapter;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.AdapterView;
-//import android.widget.ListView;
-//import android.app.Activity;
-//
-//public class BrowseFurnitureActivity extends AppCompatActivity {
-//    public final static String SELECTED_FURNITURE = "com.example.deon.furnituar.BrowseFurnitureActivity.SELECTED_FURNITURE";
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_browse_furniture);
-//        Log.d("onCreate", "inside onCreate");
-//       if (savedInstanceState == null) {
-//           PlaceholderFragment newFragment = new PlaceholderFragment();
-//           getSupportFragmentManager().beginTransaction()
-//                   .add(R.id.furniture_container, newFragment)
-//                   .commit();
-//       }
-//    }
-//
-//    public static class PlaceholderFragment extends Fragment {
-//        private ArrayAdapter<String> furnitureListAdapter;
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//            String[] tableList = {
-//                   "table 1",
-//                   "table 2",
-//                   "table 3",
-//                   "table 4",
-//                   "table 5",
-//                   "table 6"};
-//
-//           List<String> furnitureList = new ArrayList<String>(Arrays.asList(tableList));
-//           furnitureListAdapter = new ArrayAdapter<String>(
-//                   getActivity(),
-//                   R.layout.browse_furniture_list_item,
-//                   R.id.text_view_furniture_description,
-//                   furnitureList);
-//
-//            View rootView = inflater.inflate(R.layout.fragment_browse_furniture, container, false);
-//            ListView furniture_listView = (ListView) rootView.findViewById(R.id.list_view_furniture);
-//            furniture_listView.setAdapter(furnitureListAdapter);
-//            furniture_listView.setOnItemClickListener(
-//                    new AdapterView.OnItemClickListener() {
-//                        @Override
-//                        public void onItemClick(AdapterView<?> parent, View view, int listIndex, long id) {
-//                            String selection = furnitureListAdapter.getItem(listIndex);
-//                            Intent loadRenderFurnitureActivity = new Intent(getActivity(), RenderFurnitureActivity.class);
-//                            loadRenderFurnitureActivity.putExtra(SELECTED_FURNITURE, selection);
-//                            startActivity(loadRenderFurnitureActivity);
-//                        }
-//                    }
-//            );
-//
-//            return rootView;
-//        }
-//
-//    }
-//}
-
+}
