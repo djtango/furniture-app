1a74450cc555f89fdeb709b67da33c960ce8423d
deonjtan@gmail.com <deonjtan@gmail.com>
2016-01-07 10:52:26 +0000

    manual merge of front page

--- FurnituAR/app/src/main/java/com/example/deon/furnituar/MainActivity.java
+++ FurnituAR/app/src/main/java/com/example/deon/furnituar/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.deon.furnituar;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        ImageView animatedImage = (ImageView) findViewById(R.id.animated_image);
+        animatedImage.setBackgroundResource(R.drawable.spinning_chair);
+
+        AnimationDrawable frameAnimation = (AnimationDrawable) animatedImage.getBackground();
+
+        frameAnimation.start();
     }
 
     public void browseFurniture(View view) {
